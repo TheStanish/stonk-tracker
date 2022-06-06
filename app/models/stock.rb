74e0fc190d8b26c8669a845af31937cfefe6bcf8
TheStanish <tstan2310@gmail.com>
2022-06-06 11:32:40 -0400

    refactored code by adding results block to a partial.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,7 @@ class Stock < ApplicationRecord
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name , last_price: client.price(ticker_symbol))
     rescue => exception
-     return nil
+      return nil
     end
   end
 end
